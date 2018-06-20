c919b156d87dad4f1f489a561901b35c266ebe21
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-06-20 19:42:05 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,7 +12,7 @@ def input_to_index(index)
   index - 1 
 end
 
-def move(board = [" "] * 9, user_input, character)
+def move(board, user_input, character)
 
   board[input_to_index(user_input)] = "character"
 end
